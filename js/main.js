139e941539f8c2ee1f92c3a97959d0ae624cd9e5
PriscoCoppola <priscocoppola7@gmail.com>
2021-04-22 15:12:54 +0200

    add js based

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-console.log(Vue);
+/******************************************
+* KAWASAKI TODO LIST
+******************************************/
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        todos: [
+            {
+                text: 'Fit the drain',
+                completed: false,
+            },
+            {
+                text: 'Fit the light',
+                completed: false,
+            },
+            {
+                text: 'To refuel',
+                completed: false,
+            },
+            {
+                text: 'Check the oil',
+                completed: false,
+            },
+        ],
+        newTodo: '',
+    },
+
+    methods: {
+        // Add Todo on the list
+        addTodo() {
+            this.todos.push({
+                text: this.newTodo,
+                completed: false,
+            })
+
+            this.newTodo = '';
+        },
+
+        // Remove ToDo on the list
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        }
+
+    }
+})
