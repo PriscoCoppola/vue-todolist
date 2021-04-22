428b8a0654f51b1415c83a88572c07e2d2352b21
PriscoCoppola <priscocoppola7@gmail.com>
2021-04-22 21:19:16 +0200

    fix edit

--- js/main.js
+++ js/main.js
@@ -24,6 +24,11 @@ const root = new Vue({
             },
         ],
         newTodo: '',
+        editTodo: {
+            visibility: false,
+            text: '',
+            index: null,
+        },
     },
 
     methods: {
@@ -47,6 +52,27 @@ const root = new Vue({
         // Update ToDo on the list
         updateTodo(index) {
             this.todos[index].completed = ! this.todos[index].completed
+        },
+
+        // Edit ToDo on the list
+        edit(index) {
+            this.editTodo.text = this.todos[index].text;  
+            this.editTodo.index = index;
+            this.editTodo.visibility = true;
+        },
+
+        // Update Todo on the list
+        updateTodo() {
+            this.todos[this.editTodo.index].text = this.editTodo.text;
+
+            this.cancelTodo();
+        },
+
+        // Cancel ToDo on the list
+        cancelTodo() {
+            this.editTodo.text = '';  
+            this.editTodo.index = null;
+            this.editTodo.visibility = false;
         }
     }
 })
